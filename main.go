ab91fb2fd14c94141cd25883368a3bdc04bca0c9
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2021-01-15 00:19:22 +0900

    refactoring

--- main.go
+++ main.go
@@ -23,10 +23,20 @@ const LifecycleTransition = "autoscaling:EC2_INSTANCE_TERMINATING"
 
 func main() {
 	if strings.HasPrefix(os.Getenv("AWS_EXECUTION_ENV"), "AWS_Lambda") || os.Getenv("AWS_LAMBDA_RUNTIME_API") != "" {
-		lambda.Start(handler)
+		runAsLambda()
 	} else {
 		runAsCLI()
 	}
+}
+
+func runAsLambda() {
+	lambda.Start(func(ctx context.Context, event *events.AutoScalingEvent) error {
+		if err := handler(ctx, event); err != nil {
+			log.Println("[error]", err)
+			return err
+		}
+		return nil
+	})
 }
 
 func runAsCLI() {
@@ -47,7 +57,7 @@ func runAsCLI() {
 		},
 	}
 	if err := handler(context.Background(), event); err != nil {
-		log.Println(err.Error())
+		log.Println("[error]", err)
 		os.Exit(1)
 	}
 }
@@ -57,24 +67,33 @@ func handler(ctx context.Context, event *events.AutoScalingEvent) error {
 	if err != nil {
 		return err
 	}
-	svc := ecs.New(sess)
-	log.Printf("event: %#v", event)
+	ecsSvc := ecs.New(sess)
+	asgSvc := autoscaling.New(sess)
+	log.Printf("[info] event: %#v", event)
 
-	asgName := event.Detail["AutoScalingGroupName"].(string)
-	instanceID := event.Detail["EC2InstanceId"].(string)
-	transition := event.Detail["LifecycleTransition"].(string)
+	asgName := str(event.Detail["AutoScalingGroupName"])
+	instanceID := str(event.Detail["EC2InstanceId"])
+	transition := str(event.Detail["LifecycleTransition"])
 	log.Printf("[info] starting lifecycle hook AutoScalingGroupName:%s EC2InstanceId:%s LifecycleTransition:%s", asgName, instanceID, transition)
 
 	if transition != LifecycleTransition {
-		return fmt.Errorf("[error] unexpected transision: %s", transition)
+		return fmt.Errorf("unexpected transision: %s", transition)
 	}
-
-	cluster, err := detectECSCluster(sess, asgName)
+	cluster, err := detectECSCluster(asgSvc, asgName)
 	if err != nil {
 		return err
 	}
-	log.Printf("[info] ECS cluster detected: %s", cluster)
+	if err := drainingInstance(ecsSvc, cluster, instanceID); err != nil {
+		return err
+	}
+	if err := complate(asgSvc, event); err != nil {
+		return err
+	}
 
+	return nil
+}
+
+func drainingInstance(svc *ecs.ECS, cluster, instanceID string) error {
 	// determine the container instance arn by EC2 Instance ID
 	list, err := svc.ListContainerInstances(&ecs.ListContainerInstancesInput{
 		Cluster: &cluster,
@@ -89,56 +108,57 @@ func handler(ctx context.Context, event *events.AutoScalingEvent) error {
 	if err != nil {
 		return err
 	}
-	var containerInstanceArn *string
+	var containerInstanceArn string
 	for _, c := range desc.ContainerInstances {
-		if *c.Ec2InstanceId == instanceID {
-			containerInstanceArn = c.ContainerInstanceArn
+		if aws.StringValue(c.Ec2InstanceId) == instanceID {
+			containerInstanceArn = aws.StringValue(c.ContainerInstanceArn)
 			break
 		}
 	}
-	if containerInstanceArn == nil {
-		return fmt.Errorf("[error] cloud not determine container instance which has %s", instanceID)
+	if containerInstanceArn == "" {
+		return fmt.Errorf("could not determine container instance which has %s", instanceID)
 	}
 
-	log.Printf("[info] container instance %s", *containerInstanceArn)
+	log.Printf("[info] container instance %s", containerInstanceArn)
 
 	// draining
 	_, err = svc.UpdateContainerInstancesState(&ecs.UpdateContainerInstancesStateInput{
 		Cluster:            &cluster,
 		Status:             aws.String("DRAINING"),
-		ContainerInstances: []*string{containerInstanceArn},
+		ContainerInstances: []*string{&containerInstanceArn},
 	})
 	if err != nil {
 		return err
 	}
-	log.Printf("[info] complete UpdateContainerInstancesState(%s) to draining", *containerInstanceArn)
+	log.Printf("[info] complete UpdateContainerInstancesState(%s) to draining", containerInstanceArn)
 
 	// wait for all tasks exited
 	for {
+		time.Sleep(10 * time.Second)
 		tasks, err := svc.ListTasks(&ecs.ListTasksInput{
 			Cluster:           &cluster,
-			ContainerInstance: containerInstanceArn,
+			ContainerInstance: &containerInstanceArn,
 		})
 		if err != nil {
 			log.Printf("[warn] %s", err)
-			time.Sleep(10 * time.Second)
 			continue
 		}
 		if len(tasks.TaskArns) == 0 {
 			break
 		}
-		log.Printf("[info] %d tasks still running on %s", len(tasks.TaskArns), *containerInstanceArn)
-		time.Sleep(10 * time.Second)
+		log.Printf(
+			"[info] %d tasks still running on %s %s",
+			len(tasks.TaskArns),
+			instanceID,
+			containerInstanceArn,
+		)
 	}
-	log.Printf("[info] all tasks exited on %s", *containerInstanceArn)
-
-	// TODO complete-lifecycle-action
+	log.Printf("[info] all tasks exited on %s %s", instanceID, containerInstanceArn)
 	return nil
 }
 
-func detectECSCluster(sess *session.Session, asgName string) (string, error) {
+func detectECSCluster(svc *autoscaling.AutoScaling, asgName string) (string, error) {
 	var cluster string
-	svc := autoscaling.New(sess)
 	out, err := svc.DescribeAutoScalingGroups(&autoscaling.DescribeAutoScalingGroupsInput{
 		AutoScalingGroupNames: []*string{aws.String(asgName)},
 	})
@@ -156,5 +176,28 @@ func detectECSCluster(sess *session.Session, asgName string) (string, error) {
 	if cluster == "" {
 		return "", errors.New("failed to detect cluster from ASG tag Key=cluster")
 	}
+	log.Printf("[info] ECS cluster detected: %s", cluster)
 	return cluster, nil
+}
+
+func complate(svc *autoscaling.AutoScaling, event *events.AutoScalingEvent) error {
+	if event.Detail["LifecycleActionToken"] == nil {
+		log.Println("[info] skip complete")
+		return nil
+	}
+	_, err := svc.CompleteLifecycleAction(&autoscaling.CompleteLifecycleActionInput{
+		AutoScalingGroupName:  aws.String(str(event.Detail["AutoScalingGroupName"])),
+		InstanceId:            aws.String(str(event.Detail["EC2InstanceId"])),
+		LifecycleActionResult: aws.String("CONTINUE"),
+		LifecycleActionToken:  aws.String(str(event.Detail["LifecycleActionToken"])),
+		LifecycleHookName:     aws.String(str(event.Detail["LifecycleHookName"])),
+	})
+	return err
+}
+
+func str(v interface{}) string {
+	if s, ok := v.(string); ok {
+		return s
+	}
+	return ""
 }
